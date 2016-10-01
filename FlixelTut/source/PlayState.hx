98559ea5c409414252f4052551331a3c2e9831a6
Vladislav Gasanov <kosmoflyko@gmail.com>
2016-10-02 02:39:22 +0300

    Add player and tilemap

--- FlixelTut/source/PlayState.hx
+++ FlixelTut/source/PlayState.hx
@@ -3,23 +3,51 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
 import flixel.text.FlxText;
+import flixel.tile.FlxTilemap;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import flixel.FlxObject;
 
 class PlayState extends FlxState
 {
 	private var _player:Player;
+	private var _map:FlxOgmoLoader;
+	private var _mWalls:FlxTilemap;
 	
 	override public function create():Void
 	{
-		_player = new Player(20, 20);
+		_map = new FlxOgmoLoader(AssetPaths.room_001__oel);
+		_mWalls = _map.loadTilemap(AssetPaths.tiles__png, 16, 16, "walls");
+		_mWalls.follow();
+		_mWalls.setTileProperties(1, FlxObject.NONE);
+		_mWalls.setTileProperties(2, FlxObject.ANY);
+		add(_mWalls);
+		
+		_player = new Player();
+		
+		_map.loadEntities(placeEntities, "entities");
+		
 		add(_player);
+		
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		FlxG.collide(_player, _mWalls);
+	}
+	
+	private function placeEntities(entityName:String, entityData:Xml):Void
+	{
+		var x:Int = Std.parseInt(entityData.get("x"));
+		var y:Int = Std.parseInt(entityData.get("y"));
+		if (entityName == "player")
+		{
+			_player.x = x;
+			_player.y = y;
+		}
 	}
 }
--- FlixelTut/source/Player.hx
+++ FlixelTut/source/Player.hx
@@ -4,6 +4,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.math.FlxPoint;
+import flixel.FlxObject;
 
 /**
  * ...
@@ -17,8 +18,16 @@ class Player extends FlxSprite
 	public function new(?X:Float=0, ?Y:Float=0) 
 	{
 		super(X, Y);
-		makeGraphic(16, 16, FlxColor.BLUE);
+		loadGraphic(AssetPaths.player__png, true, 16, 16);
+		setFacingFlip(FlxObject.LEFT, false, false);
+		setFacingFlip(FlxObject.RIGHT, true, false);
+		animation.add("lr", [3, 4, 3, 5], 6, false);
+		animation.add("u", [6, 7, 6, 8], 6, false);
+		animation.add("d", [0, 1, 0, 2], 6, false);
 		drag.x = drag.y = 160;
+		
+		setSize(8, 14);
+		offset.set(4, 2);
 		
 	}
 	
@@ -48,6 +57,7 @@ class Player extends FlxSprite
 					mA -= 45;
 				else if (_right)
 					mA += 45;
+				facing = FlxObject.UP;
 			}
 			else if (_down)
 			{
@@ -56,14 +66,33 @@ class Player extends FlxSprite
 					mA += 45;
 				else if (_right)
 					mA -= 45;
+				facing = FlxObject.DOWN;
 			}
 			else if (_left)
+			{
 				mA = 180;
+				facing = FlxObject.LEFT;
+			}
 			else if (_right)
+			{
 				mA = 0;
-				
+				facing  = FlxObject.RIGHT;
+			}
 			velocity.set(speed, 0);
 			velocity.rotate(FlxPoint.weak(0, 0), mA);
+			
+			if ((velocity.x != 0 || velocity.y != 0) && touching == FlxObject.NONE)
+			{
+				switch (facing)
+				{
+					case FlxObject.LEFT, FlxObject.RIGHT:
+						animation.play("lr");
+					case FlxObject.UP:
+						animation.play("u");
+					case FlxObject.DOWN:
+						animation.play("d");
+				}
+			}
 		}
 		
 	}
